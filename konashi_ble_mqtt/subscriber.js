dfcb82731c5ea4368739c28d1373727043a7c65b
nocd5 <nocd5rd@gmail.com>
2015-03-04 00:44:00 +0900

    cosmetic update

--- konashi_ble_mqtt/subscriber.js
+++ konashi_ble_mqtt/subscriber.js
@@ -1,5 +1,5 @@
-var mqtt = require('mqtt')
-  , client = mqtt.connect('mqtt://admin:password@192.168.2.2:61613');
+var mqtt = require('mqtt'),
+    client = mqtt.connect('mqtt://admin:password@192.168.2.2:61613');
 var WsServer = require('ws').Server;
 
 var konashiWs = new WsServer({
@@ -15,7 +15,7 @@ client.on('message', function(topic, message) {
   if(t[1] == 'Uzuki') {
     console.log(topic + ": " + message);
     var dataAry = [data["date"], data["temp"], data["rh"]];
-    konashiWs.clients.forEach(function(c) {
+    konashiWs.clients.forEach(function(c){
       c.send(JSON.stringify(dataAry));
     });
   }
