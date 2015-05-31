6909d0969d3373f71fcfaf0b5c1ec359ce37c708
nocd5 <nocd5rd@gmail.com>
2015-06-01 01:23:00 +0900

    Fix WebSocket reconnect action

--- konashi_ble_mqtt/src/script/ws.js
+++ konashi_ble_mqtt/src/script/ws.js
@@ -10,12 +10,12 @@ var _WebSocket = {
     this.ws = new(window.WebSocket || window.MozWebSocket)(this.host, "websocket");
     this.ws.addEventListener('open', function(event){
       console.log("connected");
-      this.retryCount = 0;
+      that.retryCount = 0;
     });
     this.ws.addEventListener('close', function(event){
       console.log("disconnected");
       that.retryCount = 0;
-      if ((this.autoReconnect === true) && (that.maxRetry > that.retryCount++)){
+      if ((that.autoReconnect === true) && (that.maxRetry > that.retryCount++)){
         console.log("retry count : " + that.retryCount);
         that.initialize();
       }
